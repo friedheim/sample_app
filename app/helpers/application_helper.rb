b1d5442488124df849e9240533162d55afe23638
Friedheim <huettie-mail@gmx.de>
2011-08-09 21:29:23 -0500

    chapter 4 static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def title
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if !@title.nil?
+		#	base_title += " | "+@title
+			base_title = "#{base_title} |+ #{@title}"
+		end
+		base_title
+	end
 end
