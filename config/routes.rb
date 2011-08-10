0f09a162cb3cab17bd6ac52ff887ffb423f02417
Friedheim <huettie-mail@gmx.de>
2011-08-09 23:04:11 -0500

    Added some layout stuff with css & Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+#  get "pages/home"
+#  get "pages/contact"
+#  get "pages/about"
+#  get "pages/help"
 
-  get "pages/contact"
+   
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
   
-  get "pages/about"
+  root :to => 'pages#home'
   
-  get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
