88f140d625129385ca3f1533a69e33cac3a9c06a
Friedheim <huettie-mail@gmx.de>
2011-08-10 19:19:35 -0500

    Finished layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
 	def title
 		base_title = "Ruby on Rails Tutorial Sample App"
 		if !@title.nil?
-		#	base_title += " | "+@title
-			base_title = "#{base_title} |+ #{@title}"
+		#	base_title += " | "@title
+			base_title = "#{base_title} | #{@title}"
 		end
 		base_title
 	end
