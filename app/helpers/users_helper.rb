007c0559c6b5f58d35db01065ded860e2ccd0820
Friedheim <huettie-mail@gmx.de>
2011-08-11 21:53:43 -0500

    User model with passwords, and user show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,10 @@ module UsersHelper
 	def logo
 		image_tag("logo.png", :alt => "Sample App", :class => "round")
 	end
+	
+	def gravatar_for(user, options = { :size => 50})
+	  gravatar_image_tag(user.email.downcase, :alt => user.name,
+                                            :class => 'gravatar',
+                                            :gravatar => options)
+  end
 end
